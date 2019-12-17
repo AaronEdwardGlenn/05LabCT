8c47f72f96c4042c585bcde0ba3ac2b7ea6c628a
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2019-12-17 15:58:20 -0800

    travis how dare u

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -18,75 +18,74 @@ describe('app routties', () => {
 
     it('creates my bear on POST', () => {
         return request(app)
-        .post('/bears')
-        .send({Species: 'Black', Habitat: 'Forest'})
-        .then(res => {
-            expect(res.body).toEqual({
-                _id: expect.any(String), 
-                Species: 'Black',
-                Habitat: 'Forest',
-                __v: 0
+            .post('/bears')
+            .send({ Species: 'Black', Habitat: 'Forest' })
+            .then(res => {
+                expect(res.body).toEqual({
+                    _id: expect.any(String), 
+                    Species: 'Black',
+                    Habitat: 'Forest',
+                    __v: 0
+                });
             });
-        });
     });
 
     it('gets the bears on GET', async() => {
-    const bears = await Bear.create([
-    { Species: 'Grizzly', Habitat: 'Glacier NP'},
-    { Species: 'Green', Habitat: 'Snow NP'},
-    { Species: 'Yellow', Habitat: 'Ice NP'}
-    ]); 
-    return request(app)
-   .get('/bears')
-   .then(res => {
-       bears.forEach(bear => {
-           expect(res.body).toContainEqual({
-            _id: bear._id.toString(),
-            Species: bear.Species, 
-            Habitat: bear.Habitat,
-            __v: bear.__v
-           });
-       });
-   });
-
-  });
-
-  it('gets a bear by ID on GET', async() => {
-    const bear = await Bear.create({
-        Species: 'Brown',
-        Habitat: 'Yosemite'
-    });
-    console.log(bear);
-    
-    return request(app)
-    .get(`/bears/${bear._id}`)
-    .then(res => {
-        expect(res.body).toEqual({
-            _id: bear._id.toString(),
-            Species: bear.Species,
-            Habitat: bear.Habitat,
-            __v: bear.__v
-        });
-    });
-  });
-
-  it('updates a bear with PATCH', async() => {
-    const bear = await Bear.create({
-        Species: 'Panda',
-        Habitat: 'Bamboo Forest'
-    }); 
-
-    return request(app)
-    .patch(`/bears/${bear._id}`)
-    .send({ Species: 'Panderrrrr' })
-        .then(res => {
-            expect(res.body).toEqual({
-                _id: bear._id.toString(),
-                Species: 'Panderrrrr',
-                Habitat: 'Bamboo Forest',
-                __v: bear.__v 
+        const bears = await Bear.create([
+            { Species: 'Grizzly', Habitat: 'Glacier NP' },
+            { Species: 'Green', Habitat: 'Snow NP' },
+            { Species: 'Yellow', Habitat: 'Ice NP' }
+        ]); 
+        return request(app)
+            .get('/bears')
+            .then(res => {
+                bears.forEach(bear => {
+                    expect(res.body).toContainEqual({
+                        _id: bear._id.toString(),
+                        Species: bear.Species, 
+                        Habitat: bear.Habitat,
+                        __v: bear.__v
+                    });
+                });
             });
+
+    });
+
+    it('gets a bear by ID on GET', async() => {
+        const bear = await Bear.create({
+            Species: 'Brown',
+            Habitat: 'Yosemite'
         });
+    
+        return request(app)
+            .get(`/bears/${bear._id}`)
+            .then(res => {
+                expect(res.body).toEqual({
+                    _id: bear._id.toString(),
+                    Species: bear.Species,
+                    Habitat: bear.Habitat,
+                    __v: bear.__v
+                });
+            });
+    });
+
+    it('updates a bear with PATCH', async() => {
+        const bear = await Bear.create({
+            Species: 'Panda',
+            Habitat: 'Bamboo Forest'
+        }); 
+
+        return request(app)
+            .patch(`/bears/${bear._id}`)
+            .send({ Species: 'Panderrrrr' })
+            .then(res => {
+                expect(res.body).toEqual({
+                    _id: bear._id.toString(),
+                    Species: 'Panderrrrr',
+                    Habitat: 'Bamboo Forest',
+                    __v: bear.__v 
+                });
+            });
     });
 
     it('can delete a Bear with DELETE', async() => {
@@ -95,17 +94,17 @@ describe('app routties', () => {
             Habitat: 'Eucyliptus Forest'
         }); 
 
-    return request(app)
-    .delete(`/bears/${bear._id}`)
-    .then(res => {
-        expect(res.body)
-        .toEqual({
-            _id: bear._id.toString(),
-            Species: 'Koala', 
-          Habitat: 'Eucyliptus Forest',
-          __v: 0
-        })
+        return request(app)
+            .delete(`/bears/${bear._id}`)
+            .then(res => {
+                expect(res.body)
+                    .toEqual({
+                        _id: bear._id.toString(),
+                        Species: 'Koala', 
+                        Habitat: 'Eucyliptus Forest',
+                        __v: 0
+                    });
+            });
     });
-    });
-  });
+});
 
